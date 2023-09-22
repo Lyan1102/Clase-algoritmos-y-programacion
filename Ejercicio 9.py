772e7c9a86088d2fa8b6ce39d887c47142ff8caa
Lyan1102 <144927333+Lyan1102@users.noreply.github.com>
2023-09-22 10:43:37 -0500

    python
    trabajo 9

---
+++ Ejercicio 9.py
@@ -0,0 +1,40 @@
+#Vamos a realizar un programa que realice una tupla compuesta
+#por diferetnes tipos de datos que incluyan variables
+#adicionalmente que esten orientados a realizar diferentes acciones
+#enfocadas en generar resustados en esas vibraciones
+
+#print ("Este es un programa de tuplas")
+
+#n = [(1,4), (4,8)]
+#t = [("Lyan", 1), (11, "Hogar")]
+
+#print (n, t)
+
+#res = ()
+
+#for n in res:
+#    print (n)
+#    if n == res:
+#        print (res)
+
+#Localizar dentro de una tupla la posicion determinada por un operador
+#de referencuia como el:
+#A = ("casa", "programa para posicionar tuplas en una variables", "Tuplas en una variable", "Juan" ,[2, 9, 2022])
+#print (A[:4])
+
+#for A in True:
+#    print (A)
+#    if A == True:
+#        print ("casa")
+
+
+L = ("Michi", "Cafe", "Cena", "11", "^3", "-3", "Hola", "Finn", "Jacke", "J", [0, 9, 2022])
+print (L [0:1])
+print (L [2:4])
+print (L [3:9])
+
+
+for L in True:
+    print (L)
+    if L == True:
+        print ("j")
